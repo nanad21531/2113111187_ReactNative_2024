a783b3eb6f38e798c548315fff5967ea7d01443e
nanad21531 <nanad21531@hotmail.com>
2024-08-03 12:08:37 +0700

    pratice3 3/8/2567

---
+++ App3.tsx
@@ -0,0 +1,55 @@
+import { StatusBar } from "expo-status-bar";
+import React, {useEffect, useState} from "react";
+import { StyleSheet, Text, View, Button, Alert,TextInput } from "react-native";
+import AppFooter from "./components/AppFooter";
+import AppHeader from "./components/AppHeader";
+import Content from "./components/Content";
+import { stylesPractice } from "./styles/styles";
+ 
+export default function App(): React.JSX.Element {
+
+  const [fullname, setFullname] = useState('');
+  const [message, setMessage] = useState('Message from App.tsx');
+  const [footerMessage, setFooterMessage] = useState('Thai-Nichi');
+
+  useEffect(()=>{
+    console.log("Component has mounted");
+  },[]);
+
+  useEffect(()=>{
+    console.log(`Fullname has changed to : ${fullname}`);
+  },[fullname]);//this will run whenever 'fullname' change
+
+
+  const handleButtonClick=()=>{
+    Alert.alert("Hello", `Imput your full name : ${fullname} `);
+  }
+
+
+ 
+ 
+  return (
+    <View style={styles.container}>
+      <AppHeader  fullname={fullname} message={message} />
+      <Content message={message}  onButtonClick = {handleButtonClick} />
+      <AppFooter footerMessage={footerMessage} />
+      <TextInput
+      style={stylesPractice.input}
+      placeholder="Enter your fullname"
+      value={fullname}
+      onChangeText={setFullname}
+
+      />
+    </View>
+  );
+}
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+ 
--- components/UseEffectExample.tsx
+++ components/UseEffectExample.tsx
@@ -1,9 +1,13 @@
-import { View, Text, Button} from 'react-native'
+import { View, Text, Button, Image, StyleSheet, TextInput } from 'react-native'
 import React, { useEffect, useState } from 'react'
+import { styles } from './styles'
+import { stylesLogin } from "./styles";
 
 const UseEffectExample = (): React.JSX.Element => {
     const [count1, setCount1] = useState(0);
     const [count2, setCount2] = useState(0);
+    const [name, setName] = useState("Boriboon Srianekratha");
+    const [image, setImage] = useState(require("../assets/nanad.png"));
 
     useEffect(() => {
         console.log("use Effect1 = Runs after every render")
@@ -17,18 +21,71 @@ const UseEffectExample = (): React.JSX.Element => {
         console.log("Use Effact 3")
     }, []);
 
+    const handleChangeName = () => {
+        setName(name == "Boriboon Srianekratha" ? "No name" : "Boriboon Srianekratha");
+    }
+    const handleChangeimage = () => {
+        setImage(image == require("../assets/cat.jpg") ? require("../assets/nanad.png") : require("../assets/cat.jpg"));
+    }
 
 
 
     return (
-        <View style={{ marginTop: 50 }}>
-            <Text style={{ marginTop: 50 }}>Count1: {count1}</Text>
-            <Button title="Increment Count1" onPress={() => setCount1(count1 + 1)} />
-            <Text>{"\n\n"}</Text>
-            <Text>Count2: {count2}</Text>
-            <Button title="Increment Count2" onPress={() => setCount2(count2 + 1)} />
+        <View style={styles.container}>
+            <View style={styles.profileContainer}>
+
+                <Image style={styles.profileImage} source={image} />
+                <View>
+                    <Text style={styles.profileName}>{name}</Text>
+                    <Button title='ChangeName' onPress={handleChangeName} />
+                    <Text>{"\n"}</Text>
+                    <Button title='Changeimage' onPress={handleChangeimage} />
+
+
+                </View>
+
+
+
+            </View>
+            <View style={stylesLogin.container}>
+                <Text>{"\n"}</Text>
+                
+                <TextInput
+                    style={stylesLogin.input}
+                    
+                    
+                    placeholder="Enter your Name"
+                    
+                //  placeholder="Enter your Email"
+                //  Button title="SUBMIT" 
+                />
+                <TextInput
+                    style={stylesLogin.input}
+                    
+                    
+                    placeholder="Enter your Email"
+                    
+
+                />
+            <Button title='submit'/>
+            </View>
+
         </View>
-    );
+    )
+
+
+
+
+    // return (
+    //     <View style={{ marginTop: 50 }}>
+    //         <Text style={{ marginTop: 50 }}>Count1: {count1}</Text>
+    //         <Button title="Increment Count1" onPress={() => setCount1(count1 + 1)} />
+    //         <Text>{"\n\n"}</Text>
+    //         <Text>Count2: {count2}</Text>
+    //         <Button title="Increment Count2" onPress={() => setCount2(count2 + 1)} />
+    //     </View>
+    // );
 }
+
 
 export default UseEffectExample

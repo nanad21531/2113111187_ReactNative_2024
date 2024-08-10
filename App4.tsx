00cfe401e4cc69e1e1462a3567b202bbaee5803c
nanad21531 <nanad21531@hotmail.com>
2024-08-10 11:19:39 +0700

    Flatcallback

--- App4.tsx
+++ App4.tsx
@@ -3,14 +3,11 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import ProfileScreen from './components/ProfileScreen'
 import UseEffectExample from './components/UseEffectExample'
-import FlastListExample from './components/FlastListExample'
-import FlatListcallBackend from './components/FlatListcallBackend'
 const App = (): React.JSX.Element => {
     return (
         <View>
             {/* <UseEffectExample /> */}
-            <FlastListExample/>
-            <FlatListcallBackend/>
+            <ProfileScreen/>
         </View>
     )
 }
